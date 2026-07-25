3376686d35e8d9500cc9c3ea6b475b91816f2d49
zeets13 <jarifatasnim13@gmail.com>
2026-07-25 22:08:35 +0600

    Footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="bg-ink py-8">
       <div className="max-w-7xl mx-auto px-8 flexflex-col md:flex-row items-center">
         <p className="text-sm text-center text-gray-200 font-light tracking-wide">
-          © {new Date().getFullYear()} Jarifa Tasnim — All rights reserved.
+          © {new Date().getFullYear()} Created by Jarifa Tasnim — All rights reserved.
         </p>
         
       </div>
