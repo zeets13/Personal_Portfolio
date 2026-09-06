e3fa7141f913a857a1ae60cb551be9ba4d3780c8
zeets13 <jarifatasnim13@gmail.com>
2026-09-06 20:19:54 +0600

    Name changed in test file

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('TC-001 - portfolio homepage loads successfully', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle('jarifa-portfolio');
+  await expect(page).toHaveTitle('jarifa-tasnim');
 });
